23d3b7cf1cf1479272bfff7d1d52ba1b7248a75f
nassimbouchemella <nassimeu38950@gmail.com>
2025-07-01 11:18:10 -0400

    changement

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -8,7 +8,7 @@ const Button = ({title, icon,onClick}:{title?:string, icon?:React.ReactNode,onCl
     <div>
         <button 
         onClick={onClick}
-        className="relative w- full inline-flex h-12 overflow-hidden rounded-lg p-[1.5px] focus:outline-none md:w-60 ">
+        className="relative w-full inline-flex h-12 overflow-hidden rounded-lg p-[1.5px] focus:outline-none md:w-60 ">
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
             <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl gap-2">
                 {title}
