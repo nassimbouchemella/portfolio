3049a90eff226b4e45a416202077611049ab3e95
nassimbouchemella <Nassimeu38950@gmail.com>
2025-07-01 12:32:32 -0400

    modification de données pour les réseaux sociaux.

--- data/socials.tsx
+++ data/socials.tsx
@@ -1,10 +1,14 @@
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 export interface socials {
   name: string;
-  img: string;
+  img: React.ReactNode;
+  link: string;
   id: string;
 }
 
 export const socials = [
-  { name: "LinkedIn", icon: "https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg", id: "1"},
-  { name: "Github", icon: "https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg", id: "2" }
+  { name: "LinkedIn", icon: <FaLinkedin color='white' size={20}/>,link:'https://www.linkedin.com/in/nassim-bouchemella-ab90aa259/', id: "1"},
+  { name: "Github", icon: <FaGithub color='white' size={20}/>, link:'https://github.com/nassimbouchemella' ,id: "2" }
 ];
